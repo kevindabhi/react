9c657437ba501fb835cb16abecaf42404888197d
Kevin Dabhi <kevindabhi@gmail.com>
2022-10-07 21:05:56 +0200

    react how to display list

--- react-docs-beta/lesson-01/1/src/App.js
+++ react-docs-beta/lesson-01/1/src/App.js
@@ -1,15 +1,45 @@
- function MyButton () {
-  return (
-    <button> this is a button </button>
-  );
-}
+//  function MyButton () {
+//   return (
+//     <button> this is a button </button>
+//   );
+// }
 
-export default function MyExtendedFunction () {
+// export default function MyExtendedFunction () {
   
+//   return (
+//    <div>
+//     <h1> Hello </h1>
+//     <MyButton />
+//    </div>
+//   );
+// }
+
+
+
+//Rendering Lists
+
+const products = [
+  {title : "cabbage", id:1},
+  {title :"apple", id:2},
+  {title:"grapes", isFruit:true, id:3}, 
+
+]
+
+export default function Shoppinglist() {
+const listitems = products.map(products=>
+    <li key={products.id}
+    style = {{
+      color:products.isFruit ? "magenta" : "blue"
+    }}
+    >
+ 
+      {products.title}
+    </li>
+
+  )
+
+
   return (
-   <div>
-    <h1> Hello </h1>
-    <MyButton />
-   </div>
+    <ul>{listitems}</ul>
   );
 }
