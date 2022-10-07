0af8e0cd60582031b3ff1c5aab0b1f9f5eb88189
Kevin Dabhi <kevindabhi@gmail.com>
2022-10-07 10:21:20 +0200

    react

---
+++ react-docs-beta/lesson-01/1/src/App.js
@@ -0,0 +1,15 @@
+ function MyButton () {
+  return (
+    <button> this is a button </button>
+  );
+}
+
+export default function MyExtendedFunction () {
+  
+  return (
+   <div>
+    <h1> Hello </h1>
+    <MyButton />
+   </div>
+  );
+}
---
+++ react-docs-beta/lesson-01/lesson1.js
@@ -0,0 +1,5 @@
+function MyButton () {
+    return (
+    <button>This is a button</button>
+    );
+}
