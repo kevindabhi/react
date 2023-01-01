97cc0183d411b0315de2f81f8b8ecb29726050aa
Kevin Dabhi <kevindabhi@gmail.com>
2023-01-01 18:33:36 +0100

    updates

--- wjwuw8/src/App.js
+++ wjwuw8/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { useState } from 'react';
 
-function Square () {
+function Square() {
   const [value, setValue] = useState(null);
 
   function HandleClick() {
     setValue("X");
   }
-  
- return (
-   <button className="square" onClick={HandleClick}>{value}</button>
- );
+
+  return (
+    <button className="square" onClick={HandleClick}>{value}</button>
+  );
 }
 
 export default function Board() {
   return (
-  <div>
+    <div>
       <div className="board-row">
         <Square />
         <Square />
@@ -31,6 +31,7 @@ export default function Board() {
         <Square />
         <Square />
       </div>
-  </div>
+    </div>
+
   );
 }
